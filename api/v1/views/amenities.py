1a0305776604688be72a2b428d916ad0a3fd5b27
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-26 11:35:56 +0300

    amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -32,7 +32,7 @@ def del_a_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    storage.delete()
+    storage.delete(amenity)
     storage.save()
     return make_response(jsonify({}), 200)
 

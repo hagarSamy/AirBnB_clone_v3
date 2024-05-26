35ec5be71201b5db572093f2576941998a3f804f
hagarSamy <hs1800@fayoum.edu.eg>
2024-05-27 01:58:23 +0300

    enhancing reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -16,7 +16,7 @@ def get_reviews(place_id):
     if place is None:
         abort(404)
     reviews = storage.all(Review)
-    return jsonify([review.to_dict() for review in reviews])
+    return jsonify([review.to_dict() for review in reviews.values()])
 
 
 @app_views.route('/reviews/<review_id>',

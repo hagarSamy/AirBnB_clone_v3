aa8c29ed3869b79fe73e85a92cfbbe99bfe9b6ae
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-27 12:44:30 +0300

    debbugging

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,14 +16,14 @@ def get_cities(state_id):
     if not state:
         abort(404)
     # cities = storage.all(City)
-    cities = state.cities
-    return jsonify([city.to_dict() for city in cities])
+    citiess = state.cities
+    return jsonify([city.to_dict() for city in citiess])
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_a_city(city_id):
     '''Retrieves a City object'''
-    city = storage.get(City, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
@@ -33,7 +33,7 @@ def get_a_city(city_id):
                  strict_slashes=False)
 def del_a_city(city_id):
     '''Deletes a City object'''
-    city = storage.get(State, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
     storage.delete(city)
@@ -45,12 +45,17 @@ def del_a_city(city_id):
                  strict_slashes=False)
 def add_city(state_id):
     '''Creates a City'''
+    state = storage.get('State', state_id)
+    if not state:
+        abort(404)  # to update a city drom a state, get the state first
     if not request.get_json():
         abort(400, description="Not a JSON")
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
-    citydict = request.get_json()
-    new_city = City(state_id=state_id, name=citydict['name'])
+    #citydict = request.get_json()
+    #new_city = City(state_id=state_id, name=citydict['name'])
+    new_city = City(**new_city)
+    setattr(new_city, 'state_id', state_id)
     storage.new(new_city)
     storage.save()
     return make_response(jsonify(new_city.to_dict()), 201)
@@ -59,7 +64,7 @@ def add_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     '''Updates a City object'''
-    city = storage.get(City, city_id)
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
     if not request.get_json():
